a4c0a17dd56d1cf499b4e76d7f14670adff79069
fryoka <r.k.m.476376.mm@gmail.com>
2025-07-17 05:40:26 +0000

    passport認証の動作確認をしよう

--- routes/signup.js
+++ routes/signup.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   res.render('signup', {
     title: 'Sign up',
+    isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -16,15 +22,17 @@ router.post('/', function (req, res, next) {
   knex("users")
     .where({name: username})
     .select("*")
-    .then(function (result) {
+    .then(async function (result) {
       if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
+          isAuth: isAuth,
           errorMessage: ["このユーザ名は既に使われています"],
         }) 
       } else if (password === repassword) {
+        const hashedPassword = await bcrypt.hash(password, 10);
         knex("users")
-          .insert({name: username, password: password})
+          .insert({name: username, password: hashedPassword})
           .then(function () {
             res.redirect("/");
           })
@@ -32,12 +40,14 @@ router.post('/', function (req, res, next) {
             console.error(err);
             res.render("signup", {
               title: "Sign up",
+              isAuth: isAuth,
               errorMessage: [err.sqlMessage],
             });
           });
       } else {
         res.render("signup", {
           title: "Sign up",
+          isAuth: isAuth,
           errorMessage: ["パスワードが一致しません"],
         });
       }
@@ -46,6 +56,7 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render("signup", {
         title: "Sign up",
+        isAuth: isAuth,
         errorMessage: [err.sqlMessage],
       });
     });
