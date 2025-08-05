e30dcbf58eb55cf3290baa420408d40adc2a0133
fryoka <r.k.m.476376.mm@gmail.com>
2025-08-05 09:47:39 +0000

    カレンダー

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../db/knex');
 
 router.get('/api/tasks', (req, res) => {
   const sql = 'SELECT id, title, deadline AS start FROM tasks';
